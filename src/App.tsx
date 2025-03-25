70e550accfcc255f9fcbc867e20ca7870ed66760
Olivia Bouvier <obouvier@udel.edu>
2025-03-25 12:05:31 -0400

    deleted unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ProfessorData from './Components/ProfessorData';
 import TitleCard from './Components/TitleCard';
+
+
 
 function App() {
   
@@ -11,18 +12,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <TitleCard></TitleCard>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <ProfessorData></ProfessorData>
       </header>
     </div>
